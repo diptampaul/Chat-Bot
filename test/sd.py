5a8c36bad0972e3d67c3f3784d2be1f247143f84
diptampaul <diptampaul13@gmail.com>
2022-12-31 02:19:27 +0530

    Initial Commit

---
+++ test/sd.py
@@ -0,0 +1,16 @@
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+import torch
+torch.cuda.empty_cache()
+
+model_id = "stabilityai/stable-diffusion-2-1"
+
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe = pipe.to("cuda")
+print("Cuda loaded")
+pipe.enable_attention_slicing()
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+    
+image.save("astronaut_rides_horse.png")
